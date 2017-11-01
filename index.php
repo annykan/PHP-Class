af278224a1cab9217cd6d2f7b2ffde3f8336ec99
Anny <anny_phison@hotmail.com>
2017-11-01 15:25:22 +0800

    第一個 php 語法

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello | PHP</title>
+</head>
+<body>
+<H1>Hello World~from PHP</H1>
+<H1>«¢Åo~from PHP</H1>
+<?php
+	echo "This is after hypertext preprocessor!"
+?>
+</body>
+</html>
