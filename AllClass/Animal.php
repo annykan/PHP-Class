2ad11c9e6c553f641d25df79c6c8c102447a488a
Anny <anny_phison@hotmail.com>
2017-11-01 16:57:24 +0800

    把動物共同會做的事放在父類別

--- AllClass/Animal.php
+++ AllClass/Animal.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 //1. 定義 Animal 類別
 //2. 屬性 position 可被繼承
@@ -6,21 +6,53 @@
 //4. 可以取得物種名稱
 //5. 可以從外部得到食物
 
-class Animal
-{
-    private $animalType = "default animal Type";//若沒有呼叫父類別的建構子,會印出這個
-    protected $position;
+class Animal {
 
-    public function __construct($animalTypeFromOut) {
+    Protected $position;
+    Private $animalType="default animal Type";//若沒有呼叫父類別的建構子,會印出這個
+
+    public function __construct($animalTypeFromOut)
+    {
+        $this->position = ["x"=>0 , "y"=>0];
         $this->animalType = $animalTypeFromOut;
-        $this->position = [ "x" => 0, "y" => 0 ];
     }
 
-    public function getAnimalType() {
+    public function getAnimalType()
+    {
         return $this->animalType;
+
     }
 
-    public function getFood($foodName) {
-        echo "得到食物: $foodName<br>";
+    public function getFood($foodName)
+    {
+        $this->foodName = $foodName;
+        echo "<br> Food is $foodName <br>" ;
+        $this->eat();
     }
+
+    public function eat()
+    {
+        echo "吃".$this->foodName."<br>";
+
+        $this->digest();
+    }
+
+    function digest()
+    {
+        echo "消化".$this->foodName."<br>";
+    }
+
+    public function moveDxDy($dx, $dy){
+        $this->position["x"] += $dx ;
+        $this->position["y"] += $dy ;
+        $this->getPosition();
+    }
+
+    public function getPosition(){
+        $x = $this->position["x"];
+        $y = $this->position["y"];
+        echo "<br> Now position : ($x, $y)<br>";
+    }
+
+
 }
